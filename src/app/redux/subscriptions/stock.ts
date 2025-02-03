55e3615e99f9904bfc84768397227ccc8b5da5fa
isaac <isaac>
2025-02-03 18:40:22 +0100

    chore: modified env naming

--- src/app/redux/subscriptions/stock.ts
+++ src/app/redux/subscriptions/stock.ts
@@ -8,7 +8,7 @@ import {
 export const stockApi = createApi({
 	reducerPath: "stockApi",
 	baseQuery: fetchBaseQuery({
-		baseUrl: process.env.NEXT_PUBLIC_STOCK_BASEURL,
+		baseUrl: process.env.NEXT_PUBLIC_ALPHAVANTAGE_BASEURL,
 	}),
 	refetchOnMountOrArgChange: true,
 	refetchOnReconnect: true,
@@ -16,22 +16,22 @@ export const stockApi = createApi({
 	endpoints: (builder) => ({
 		fetchStockListings: builder.query<any, any>({
 			query() {
-				return `?function=TOP_GAINERS_LOSERS&apikey=${process.env.NEXT_PUBLIC_STOCK_APIKEY}`;
+				return `?function=TOP_GAINERS_LOSERS&apikey=${process.env.NEXT_PUBLIC_ALPHAVANTAGE_APIKEY}`;
 			},
 		}),
 		searchStockListings: builder.query<SearchStockListingResults, string>({
 			query(searchTerm) {
-				return `?function=SYMBOL_SEARCH&keywords=${searchTerm}&apikey=${process.env.NEXT_PUBLIC_STOCK_APIKEY}`;
+				return `?function=SYMBOL_SEARCH&keywords=${searchTerm}&apikey=${process.env.NEXT_PUBLIC_ALPHAVANTAGE_APIKEY}`;
 			},
 		}),
 		getStockDetails: builder.query<StockDetails, string>({
 			query(symbol) {
-				return `?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${process.env.NEXT_PUBLIC_STOCK_APIKEY}`;
+				return `?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${process.env.NEXT_PUBLIC_ALPHAVANTAGE_APIKEY}`;
 			},
 		}),
 		getStockBySymbol: builder.query<Stock | null, string>({
 			query: (symbol) =>
-				`?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.NEXT_PUBLIC_STOCK_APIKEY}`,
+				`?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.NEXT_PUBLIC_ALPHAVANTAGE_APIKEY}`,
 			transformResponse: (response: any): Stock | null => {
 				const quote = response["Global Quote"];
 				if (!quote) return null;
